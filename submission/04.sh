067ccbf881321f2370c56148118e59dd3fb2b090
Sammie05 <snweke98@gmail.com>
2025-04-22 01:17:29 +0100

    lbcli-week-5-exercises

--- submission/04.sh
+++ submission/04.sh
@@ -1,7 +1,7 @@
 # Create a CLTV script with a timestamp of 1495584032 and public key below:
 # publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 
-publicKey="02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277"
+public_Key="02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277"
 
 timestamp=1495584032
 
@@ -28,7 +28,7 @@ TIMESTAMP_BYTES=$(echo -n "$TIMESTAMP_LE_HEX" | wc -c | awk '{print $1/2}')
 TsIMESTAMP_PUSH=$(printf "%02x" $TIMESTAMP_BYTES)
 
 
-PUBKEY_HASH=$(echo -n "$PUBLIC_KEY" | xxd -r -p | openssl dgst -sha256 -binary | openssl dgst -rmd160 -binary | xxd -p)
+PUBKEY_HASH=$(echo -n "$public_key" | xxd -r -p | openssl dgst -sha256 -binary | openssl dgst -rmd160 -binary | xxd -p)
 
 CRIPT_HEX="${TIMESTAMP_PUSH}${TIMESTAMP_LE_HEX}${OP_CHECKLOCKTIMEVERIFY}${OP_DROP}${OP_DUP}${OP_HASH160}${PUBKEY_HASH_PUSH}${PUBKEY_HASH}${OP_EQUALVERIFY}${OP_CHECKSIG}"
 
